0491284e53bb2f726ea0a476b662593b24fb7fbf
fredrikbk <fredrikbk@gmail.com>
2012-12-02 13:08:31 -0500

    Fixed

--- project/nb_predict.m
+++ project/nb_predict.m
@@ -1,13 +1,14 @@
-function [ prediction ] = nb_predict( wordcounts, theta )
-    k = length(theta(:,1));
+function [ prediction ] = nb_predict( X, theta )
+    X
+
+    K = length(theta(:,1));
     D = length(theta(1,:));
 
-    pr = zeros(k, 1);
-    for c=1:k
+    pr = zeros(K, 1);
+    for c=1:K
         for j=1:D
-            x_j = wordcounts(j);
-            t_cj = theta(c,j);
-            prxj = (x_j*log(t_cj) + (1 - x_j)*log(1-t_cj));
+            t_cj = theta{c}(j);
+            prxj = (X(j)*log(t_cj) + (1 - X(j))*log(1-t_cj));
             pr(c) = pr(c) + prxj;
         end
     end
