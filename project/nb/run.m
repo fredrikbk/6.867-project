324b074dd4e2f25fd206f09bb0de45131a5572a1
Christopher R. Johnson <crjohns@csail.mit.edu>
2012-12-02 16:55:22 -0500

    Fixed run percentage counting

--- project/nb/run.m
+++ project/nb/run.m
@@ -4,10 +4,12 @@ size = count0 + count1;
 pi = [count0 / size, count1 / size];
 
 correct = 0;
+total = 0;
 for i=1:249
     predict = nb_predict(full(sparseX(i,:)), theta, pi);
 %    fprintf(1,'%d, %d\n',predict, Y(i));
     correct = correct + (predict == Y(i));
+    total = total + 1;
 end
 
-fprintf(1, '%f\n', correct / size);
+fprintf(1, '%f\n', correct / total);
