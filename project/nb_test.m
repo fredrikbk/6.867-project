410d628073b864b3865ffb55cd7e3f920902df17
fredrikbk <fredrikbk@gmail.com>
2012-12-02 13:08:31 -0500

    Fixed

--- project/nb_test.m
+++ project/nb_test.m
@@ -1,26 +1,44 @@
-words = {'August', 'hallo', 'hei', 'hello', 'hey', 'ja', 'mister', 'yes'};
+words = {'August', 'hallo', 'hei', 'hello', 'hey', 'ja', 'mister', 'yes'}
 D = length(words);
 
-test_counts = zeros(2, D);
-test_counts(1,:) = [0 3 1 0 1 4 1 0]; % Norwegian (C=0)
-test_counts(2,:) = [2 0 0 3 2 0 1 1]; % English   (C=1)
+examples = cell(2);
 
-theta = nb_train(test_counts);
+% Norwegian (C=0)
+norwegian_examples = struct('num_training_vectors', 5, 'training_vector_sums', sparse(1,D));
+norwegian_examples.training_vector_sums = [1 3 1 0 1 4 1 0]
+examples{1} = norwegian_examples;
 
-norwegian = [1 2 1 0 1 2 0 0]; % Norwegian (C=0)
+% English (C=1)
+english_examples = struct('num_training_vectors', 4, 'training_vector_sums', sparse(1,D));
+english_examples.training_vector_sums   = [2 0 0 3 2 0 1 2]
+examples{2} = english_examples;
+
+theta = nb_train(examples);
+norwegian_theta = theta{1}
+english_theta = theta{2}
+
+norwegian = [1 1 1 0 0 1 0 0]; % Norwegian (C=0)
 fprintf(1, '\nValidating on Norwegian dataset\n');
 prediction = nb_predict(norwegian, theta);
 if prediction == 0
-    fprintf(1, 'Predicted Norwegian\n');
+    fprintf(1, 'Predicted     Norwegian\n');
 else
-    fprintf(1, 'Predicted English\n');
+    fprintf(1, 'Predicted     English\n');
 end
 
-english = [2 0 0 2 1 1 1 1]; % English (C=1)
+english = [1 0 0 1 1 1 1 1]; % English (C=1)
 fprintf(1, '\nValidating on English dataset\n');
 prediction = nb_predict(english, theta);
 if prediction == 0
-    fprintf(1, 'Predicted Norwegian\n');
+    fprintf(1, 'Predicted     Norwegian\n');
 else
-    fprintf(1, 'Predicted English\n');
+    fprintf(1, 'Predicted     English\n');
 end
+
+clear words
+clear D
+clear test_counts
+clear theta
+clear norwegian
+clear english
+clear prediction
--- project/nb_train.m
+++ project/nb_train.m
@@ -1,19 +1,19 @@
-function [ theta ] = nb_train( wordcounts )
-    k = length(wordcounts(:,1));
-    D = length(wordcounts(1,:));
-
-    theta = zeros(k, D);
-    for c = 1:k
-        numc = sum(wordcounts(c,:));
-        for j = 1:D
-            theta(c, j) = (wordcounts(c,j) + 1) / (numc + 2);
+function [ theta ] = nb_train( examples )
+    assert(length(examples{1}.training_vector_sums) == length(examples{2}.training_vector_sums));
+    K = length(examples);
+    D = length(examples{1}.training_vector_sums);
+    
+    theta = cell(K);
+    for c=1:K
+        theta{c} = zeros(1,D);
+        num_yc = examples{c}.num_training_vectors;
+        nz_idx = find(examples{c}.training_vector_sums);
+        for tmp=1:length(nz_idx)
+            j = nz_idx(tmp);
+            num_xj1_yc = examples{c}.training_vector_sums(j);
+            theta{c}(j) = (num_xj1_yc + 1) / (num_yc + 2);
         end
         
-        % Normalize probabilities
-        total = sum(theta(c,:));
-        for j = 1:D
-            theta(c, j) = theta(c, j) / total;
-        end
-        assert(sum(theta(c,:)) == 1.0);
+        theta{c}(theta{c} == 0) = 1 / (num_yc + 2);
     end
 end
