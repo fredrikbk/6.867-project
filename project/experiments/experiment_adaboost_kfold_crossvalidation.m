4a2ad7ef16f21f8cdd015705b31b9d4203951770
fredrikbk <fredrikbk@gmail.com>
2012-12-06 10:49:36 -0500

    Added a folder with scripts and results from experiments

---
+++ project/experiments/experiment_adaboost_kfold_crossvalidation.m
@@ -0,0 +1,13 @@
+M = 5;
+k = 10;
+
+fprintf('M=%d, k=%d\n\n', M, k);
+
+trainer = @(X, Y) adaboost_train(X,Y,M);
+predictor = @adaboost_predict;
+
+[avg_accuracy, accuracies, string] = kfold_crossvalidation(trainer, predictor, X, Y, k, 1);
+fprintf(string);
+accuracies = accuracies'
+
+clear M trainer predictor string;
