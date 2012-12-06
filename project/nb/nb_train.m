5fca2efb353a83cf2e1cb509f996f2c65ac6d162
fredrikbk <fredrikbk@gmail.com>
2012-12-06 13:09:58 -0500

    Refactored interface to match crossvalidation algorithms

--- project/nb/nb_train.m
+++ project/nb/nb_train.m
@@ -1,25 +1,26 @@
-function [ theta, pi ] = nb_train( X, Y )
+function [ nb ] = nb_train( X, Y )
     X(X>1) = 1;
-
+    
+    nb = struct();
     classes = classify_data(X, Y);
     
     K = length(classes);
     D = length(classes{1}.class);
     
     % pi
-    pi = zeros(1,K);
+    nb.pi = zeros(1,K);
     for c=1:K
-        pi(c) = classes{c}.count/length(Y);
+        nb.pi(c) = classes{c}.count/length(Y);
     end
     
     % theta
-    theta = cell(1,K);
+    nb.theta = cell(1,K);
     for c=1:K
-        theta{c} = zeros(1,D);
+        nb.theta{c} = zeros(1,D);
         num_yc = classes{c}.count;
         for j=1:length(classes{c}.class)
             num_xj1_yc = classes{c}.class(j);
-            theta{c}(j) = (num_xj1_yc + 1) / (num_yc + 2);
+            nb.theta{c}(j) = (num_xj1_yc + 1) / (num_yc + 2);
         end
     end
 end
