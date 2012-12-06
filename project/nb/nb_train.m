6e0c351d0eb6de527dbc476c50af4504a20ab3f7
fredrikbk <fredrikbk@gmail.com>
2012-12-06 13:39:43 -0500

    ..

--- project/nb/nb_train.m
+++ project/nb/nb_train.m
@@ -12,15 +12,15 @@ function [ nb ] = nb_train( X, Y )
     for c=1:K
         nb.pi(c) = classes{c}.count/length(Y);
     end
+    %nb.pi = [1/2 1/2]
     
     % theta
-    nb.theta = cell(1,K);
+    nb.theta = zeros(K,D);
     for c=1:K
-        nb.theta{c} = zeros(1,D);
         num_yc = classes{c}.count;
         for j=1:length(classes{c}.class)
             num_xj1_yc = classes{c}.class(j);
-            nb.theta{c}(j) = (num_xj1_yc + 1) / (num_yc + 2);
+            nb.theta(c,j) = (num_xj1_yc + 1) / (num_yc + 2);
         end
     end
 end
