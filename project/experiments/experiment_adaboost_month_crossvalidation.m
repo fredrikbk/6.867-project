0c39fa6f8c3fca8df689f2647872c0a9de7ccbff
fredrikbk <fredrikbk@gmail.com>
2012-12-06 10:49:36 -0500

    Added a folder with scripts and results from experiments

---
+++ project/experiments/experiment_adaboost_month_crossvalidation.m
@@ -0,0 +1,12 @@
+M = 5;
+
+fprintf('M=%d\n\n', M);
+
+trainer = @(X, Y) adaboost_train(X,Y,M);
+predictor = @adaboost_predict;
+
+[avg_accuracy, accuracies, string] = crossvalidation(trainer, predictor, X, Y, months, 1);
+fprintf(string);
+accuracies = accuracies'
+
+clear M trainer predictor string;
