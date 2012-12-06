622e0d84b56b1edf972a1e6ba7d240e53428a0f1
fredrikbk <fredrikbk@gmail.com>
2012-12-06 08:34:49 -0500

    Some changes to the read_data script

--- project/nb/run_nb.m
+++ project/nb/run_nb.m
@@ -1,4 +1,7 @@
-theta = nb_train(examples);
+theta = nb_train(classes);
+
+count0 = classes{1}.num_training_vectors;
+count1 = classes{2}.num_training_vectors;
 
 size = count0 + count1;
 pi = [count0 / size, count1 / size];
@@ -6,10 +9,10 @@ pi = [count0 / size, count1 / size];
 correct = 0;
 total = 0;
 for i=1:249
-    predict = nb_predict(full(sparseX(i,:)), theta, pi);
+    predict = nb_predict(full(X(i,:)), theta, pi);
 %    fprintf(1,'%d, %d\n',predict, Y(i));
     correct = correct + (predict == Y(i));
     total = total + 1;
 end
 
-fprintf(1, '%f\n', correct / total);
+fprintf(1, '%.2f%%\n', 100*(correct / total));
