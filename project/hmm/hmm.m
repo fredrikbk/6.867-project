9cfbe34ff4f22c452d0054f7866a622d9ed38945
fredrikbk <fredrikbk@gmail.com>
2012-12-08 16:09:24 -0500

    ...

--- project/hmm/hmm.m
+++ project/hmm/hmm.m
@@ -10,25 +10,28 @@ for i=1:length(E)
 end
 
 % Create initial guesses for the EM algorithm
-Adiag  = 0.9;  % rand(1)/2 + 1/2;
-Budiag = 0.5;  % rand(1)/2 + 1/2;
+Adiag  = 0.6;  % rand(1)/2 + 1/2;
+Budiag = 0.6;  % rand(1)/2 + 1/2;
 [A_guess, B_guess] = em_init(NUM_HIDDEN_STATES, NUM_EMISSION_STATES, Adiag, Budiag);
 
 % Test and validate
 predictions = length(E);
+numpred = 0;
 numcorrect = 0;
-for i=1:length(E)-1
-    fprintf('\n\nObserved 1:%d\n-------------\n', i);
+window = 30;
+for i=window+1:length(E)-1
+    fprintf('\n\nObserved %d:%d\n-------------\n', i-window, i);
     
-    [A, B] = hmmtrain(E(1:i), A_guess, B_guess, 'Maxiterations',1000)
+    [A, B] = hmmtrain(E(i-window:i), A_guess, B_guess, 'Maxiterations',500)
     
     % Compute the posterior probabilities on Z for E(1:i)
-    pr_Z = hmmdecode(E(1:i),A,B)';
+    pr_Z = hmmdecode(E(i-window:i),A,B)';
     
-    pr_zi   = pr_Z(i,:); 
+    pr_zi   = pr_Z(length(pr_Z),:); 
     pr_zip1 = pr_zi * A;
     pr_xip1 = pr_zip1 * B
 
+    numpred = numpred + 1;
     if pr_xip1(1) >= pr_xip1(2)  %pr_xip1(1)
         fprintf('predicted: -1. Correct: %d\n', Y(i+1));
         if Y(i+1) == -1
@@ -45,7 +48,7 @@ for i=1:length(E)-1
     % Predict the next day
 %    predictions[i+1] = 
 end
-fprintf('numcorrect: %d\n', numcorrect);
+fprintf('numcorrect: %d/%d (%.2f%%)\n', numcorrect, numpred, 100*numcorrect/numpred);
 
 %numpred = length(E);
 %[Epred,Zpred] = hmmgenerate(numpred,A,B);
