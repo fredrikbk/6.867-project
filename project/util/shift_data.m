73e9751dad55a5d8c7253d477389b281b66e9fa6
Christopher R. Johnson <crjohns@csail.mit.edu>
2012-12-09 19:09:23 -0500

    fixed shifter

--- project/util/shift_data.m
+++ project/util/shift_data.m
@@ -4,12 +4,16 @@ sparseX = X;
 numR = length(sparseX(:,1));
 numC = length(sparseX(1,:));
 
+sparseAugX = spalloc(numR, numC, length(find(sparseX)));
+
 sparseAugX(1,1:numC) = zeros(1,numC);
 for i=2:numR
     sparseAugX(i, 1:numC) = sparseX(i-1,1:numC);
 end
 
 yesterday = sparseAugX;
+
+sparseAugX = spalloc(numR, numC, length(find(sparseX)));
 
 sparseAugX(numR,1:numC) = zeros(1,numC);
 for i=1:numR-1
